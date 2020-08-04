241f190d897e86b71dff139ff21299d57a5b34a8
Vicky Lin <jiajun_work@126.com>
2020-08-04 23:47:23 +0800

    [todolist][dong&&vicky] refactor: update post,get url.

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,21 +1,20 @@
 const axios = require ('axios');
-const host = "https://5e9ec500fb467500166c4658.mockapi.io"
-
 const todoBaseUrl = "/todos"
+const host = "https://5e9ec500fb467500166c4658.mockapi.io"+todoBaseUrl
 
 export default {
     getToDoList : function () {
-        return axios.get(host +"/todos")
+        return axios.get(host)
     },
 
-    putToDoList : function () {
+    putToDoList : function (id,) {
         
     },
     postToDo:function(data) {
-        return axios.post(host + "todo", data)
+        return axios.post(host, {data})
     },
     deleteToDo:function(id) {
-        return axios.delete(host + `/todos/${id}`)
+        return axios.delete(host + `/${id}`)
     }
 }
 
