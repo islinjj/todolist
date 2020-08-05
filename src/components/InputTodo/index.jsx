4b0bd4b70c2b7fe333533a8c144acf85d14a254c
vicky <jiajun_work@126.com>
2020-08-05 11:23:30 +0800

    [todolist][dong&&vicky] refactor: rename InputText to InputTodo.

--- src/components/InputTodo/index.jsx
+++ src/components/InputTodo/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { addItemAction, deleteItemAction, markItemAction } from '../../actions'
 import { connect } from 'react-redux';
-import Item from '../Item';
+import ItemList from '../ItemList';
 import Api from '../../api/Api'
-class InputText extends React.Component {
+class InputTodo extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             text: ''
         }
-        this.initList()
+        // this.initList()
     }
     initList = () => {
         let that = this
@@ -20,6 +20,7 @@ class InputText extends React.Component {
             }
         })
     }
+    
     onChange = (event) => {
         this.setState({ text: event.target.value })
     }
@@ -35,40 +36,35 @@ class InputText extends React.Component {
             console.log(res.data)
         })
     }
-
-    onDeleteItem = (index) => {
-        this.props.deleteItem(index)
-    }
-
-    onMark =(index) =>{
-        this.props.markItem(index)
-    }
     render() {
         return (
             <div>
                 <input onBlur={this.onChange} />
                 <button onClick={this.onAdd}>add</button>
                 {
-                    this.props.texts.map((val, key) => <Item
-                        key={key} index={key} text={val.content} onDelete={this.onDeleteItem} remoteId = {val.id}
-                        done={val.done} markItem={this.onMark} initList = {this.initList}
-                    />)
+                    // <ItemList />
+                    // this.props.texts.map((val, key) => <Item
+                    //     key={key} index={key} text={val.content} onDelete={this.onDeleteItem} remoteId = {val.id}
+                    //     done={val.done} markItem={this.onMark} initList = {this.initList}
+                    // />)
+                    
                 }
 
             </div>
         )
     }
 }
-const mapStateToProps = state => {
-    return { texts: state.texts };
-}
+// const mapStateToProps = state => {
+//     return { texts: state.texts };
+// }
 
-const mapDispatchToProps = ({
-    addItem: addItemAction,
-    deleteItem: deleteItemAction,
-    markItem: markItemAction
+// const mapDispatchToProps = ({
+//     addItem: addItemAction,
+//     deleteItem: deleteItemAction,
+//     markItem: markItemAction
+// })
 
-})
 
+// export default connect(mapStateToProps, mapDispatchToProps)(InputText)
 
-export default connect(mapStateToProps, mapDispatchToProps)(InputText)
+export default InputTodo
